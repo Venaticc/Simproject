12f7ec83a3d19855f129982408be9ab9dcdc881b
ad51880 <ivar.eriksson.trysell@elevmail.stockholm.se>
2026-09-15 14:28:59 +0200

    .
    Signed-off-by: ad51880 <ivar.eriksson.trysell@elevmail.stockholm.se>

--- EulerStepMethod.py
+++ EulerStepMethod.py
@@ -4,4 +4,18 @@ import sympy as sym
 import math as m
 
 from sympy.abc import x, y
-F = sym.Function('F')(x,y)
+
+def carc(q):
+    return 1/m.sqrt(q**2 + 1)
+
+length = 1
+g = 9.82
+b = carc(12)
+
+fx = m.sqrt(2*g/length*(carc(x/y)-b))*
+fy = sym.diff(Fy, y)
+print(fx, fy)
+
+t_start = 0
+t_end = 1
+for  in range(t_start, t_stop)
