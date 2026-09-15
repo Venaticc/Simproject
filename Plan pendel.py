e8be802bae694458b2bbc719f117280f63bffcf6
Jakobtestarsigfram <jakoblinder95@gmail.com>
2026-09-15 10:29:12 +0200

    test

--- Plan pendel.py
+++ Plan pendel.py
@@ -3,14 +3,20 @@ import math
 from math import *
 
 
-Y0= 10
+Y_0= 10
 L= 24
-V0= 0
-h= 0.01
+V_0= 0
+h_= 0.01
 g= 9.82
-A0 = 0
-O0 = acos(L-Y0/L)
-print(acos(L-Y0/L))
+A_0 = 0
+O_0 = acos((L-Y_0)/(L))
+x_0 = math.sin(O_0)*L
+A_0 = (-sin(O_0)*g)/L
+
+
+
+
+
 
 
 
