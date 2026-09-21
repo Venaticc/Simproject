c5dd08147c5615c008d0276d5932f21d9f054a5b
Jakobtestarsigfram <jakoblinder95@gmail.com>
2026-09-21 09:01:10 +0200

    test

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ w = 0
 h = 0.001
 g = 9.82
 l = 0.10
-
+O = ((0.1-(cos(y[0])*0.1))*g)
+print(O)
+W = [0]
+Energi = [O]
 def hogerlede(x,y,g,l):
     f = -sin(y)*g/l
     return f
@@ -19,10 +22,16 @@ for i in range(0,B):
     X = h+X
     w = w+dervitiv*h
     Y = Y+w*h
-    if Y>0.9999:
+    W.append(w)
+    if Y>0.1:
         print (Y,X)
     x.append(X)
+    Energi.append(((0.1-cos(Y)*0.1)*g+((w*0.1)**2)/2))
     y.append(Y)
 print(Y)
+plt.plot(x,Energi)
+plt.show()
+plt.plot(x,W)
+plt.show()
 plt.plot(x,y)
 plt.show()
